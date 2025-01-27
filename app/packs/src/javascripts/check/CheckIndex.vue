400cae4788b4693f480727f1b4aaa1309ad164ab
HawkingHuang <math0915@gmail.com>
2025-01-27 20:13:28 +0800

    Check頁面RWD

--- app/packs/src/javascripts/check/CheckIndex.vue
+++ app/packs/src/javascripts/check/CheckIndex.vue
@@ -38,22 +38,22 @@ const reset = () => {
   <div>
     <base-card>
       <div>
-        <section class="mx-auto my-6 grid max-w-[185rem] grid-cols-2 place-items-center">
-          <div class="w-full px-8 py-4">
+        <section class="mx-auto my-6 grid max-w-[185rem] grid-cols-2 place-items-center md:mt-0">
+          <div class="w-full px-8 py-4 md:col-span-2 md:px-2">
             <label for="original-text" class="mb-2 block text-3xl font-bold">Original</label>
             <textarea
               id="original-text"
               placeholder="Enter Traditional Chinese here..."
               v-model="userInput"
-              class="custom-scroll h-[40vh] w-[90%] resize-none overflow-auto rounded-lg border-4 border-gray-300 p-2 text-xl shadow-md"
+              class="custom-scroll h-[40vh] w-[90%] resize-none overflow-auto rounded-lg border-2 border-gray-300 p-2 text-xl shadow-md md:h-[20vh] md:w-full"
             ></textarea>
           </div>
-          <div class="w-full px-8 py-4">
+          <div class="w-full px-8 py-4 md:col-span-2 md:px-2">
             <label for="result-text" class="mb-2 block text-3xl font-bold">Corrected</label>
-            <div id="result-text" class="custom-scroll h-[40vh] w-[90%] resize-none overflow-auto whitespace-pre-line rounded-lg border-4 border-gray-300 p-2 text-left text-xl shadow-md"></div>
+            <div id="result-text" class="custom-scroll h-[40vh] w-[90%] resize-none overflow-auto whitespace-pre-line rounded-lg border-2 border-gray-300 p-2 text-left text-xl shadow-md md:h-[20vh] md:w-full"></div>
           </div>
         </section>
-        <section class="mx-8 flex gap-4">
+        <section class="mx-8 flex gap-4 md:flex-col md:items-center">
           <button class="default-btn btn-green w-[150px]" @click="examine" aria-label="Check">
             <CheckCircleOutlined />
             Examine
